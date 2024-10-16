7d376f09793e94e27cce252188f8147188f167f9
Lyn Resell <61920408+lynar13@users.noreply.github.com>
2024-10-16 22:32:37 +0100

    Adds prettier to eslint.config.mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import prettierConfig from "eslint-config-prettier";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
@@ -10,4 +10,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettierConfig,
 ];
